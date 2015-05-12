da2dc8308b54b7ec07be1a79eb7474df41a557ac
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2015-05-12 20:12:22 -0300

    Add arp-scan and remove zemap

--- run_ubuntu_toolbox.sh
+++ run_ubuntu_toolbox.sh
@@ -18,7 +18,7 @@ PACKAGES_ESSENTIALS="build-essential gparted ssh vim aptitude inotail subversion
                      openjdk-8-jdk idle ruby2.1 cups-pdf libsdl1.2debian libcups2 samba 
                      samba-common cifs-utils wine vlc unity-tweak-tool ubuntu-restricted-extras 
                      cowsay fortunes-br openssh-client openssh-server tree gtkterm sl mtd-tools 
-		     mkcramfs zenmap nmap"
+		     mkcramfs nmap arp-scan"
 
 LAMP="apache2 mysql-server php5-mysql mysql-workbench php5 libapache2-mod-php5 php5-mcrypt"
  
